960e796bc65e16bf87c6850d90ba4155bdca41d6
Dipali-cpu <dipalichothmal16@gmail.com>
2026-01-15 12:30:22 +0530

    Remove password and optimize app performance

--- app.py
+++ app.py
@@ -7,28 +7,7 @@ from io import BytesIO
 import base64
 from pypdf import PdfReader
 
-# Password protection
-def check_password():
-    """Returns `True` if the user had the correct password."""
-    def password_entered():
-        if hashlib.sha256(st.session_state["password"].encode()).hexdigest() == hashlib.sha256(st.secrets["password"].encode()).hexdigest():
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]
-        else:
-            st.session_state["password_correct"] = False
 
-    if "password_correct" not in st.session_state:
-        st.text_input("Password", type="password", on_change=password_entered, key="password")
-        return False
-    elif not st.session_state["password_correct"]:
-        st.text_input("Password", type="password", on_change=password_entered, key="password")
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        return True
-
-if not check_password():
-    st.stop()
 
 # All available projects
 ALL_PROJECTS = [
